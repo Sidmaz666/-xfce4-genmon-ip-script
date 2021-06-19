34709004e9e2da6638e749a7234995a998c652c7
Sidmaz666 <81381178+Sidmaz666@users.noreply.github.com>
2021-06-20 03:23:56 +0530

    Add files via upload

---
+++ ipi.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#####################################################################################################################################################
+## Aurthor :    Sidhartha Mazumder
+## Description : Very simple xfce4-genmon-plugin script to show country name/initials, city name along with the respective counttry flag and IP adress in the tooltip. 
+#####################################################################################################################################################
+
+## Get Country and City Infod
+cc=$(curl -s ifconfig.co/country);
+ccc=$(curl -s ifconfig.co/city);
+
+# Get IP Address
+ipa=$(echo "P: " && curl -s ipinfo.io/ip && echo " H: " && hostname -I | awk '{print $1}'
+);
+#Get Country Initials
+cci=$(echo ${cc} | cut -c1-2);
+
+#Make Country initials lowercase
+gfccfi=$(echo ${cci} | tr '[:upper:]' '[:lower:]');
+
+#Directory Location, Portable and Finding the Flag icon
+readonly cdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)";
+readonly flgicon="${cdir}/flags/$gfccfi.png";
+
+#Compliling Outputs
+opc=$(echo "<txt> $cci </txt><img>$flgicon</img><tool> $ipa L: $cc, $ccc</tool>");
+#Echoing Outputs
+echo $opc;
